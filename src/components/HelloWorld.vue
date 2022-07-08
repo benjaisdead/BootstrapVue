da55d20c21e68e1b91f1f6308dc6f43c39904103
benjaisdead <40874757+benjaisdead@users.noreply.github.com>
2022-07-08 04:02:17 -0400

    Desafio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="danger">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+
+
+    <div>
+      <b-jumbotron header="Soy Megatron" lead="Soy el mejor">
+        <p>Eliminare a Optimus</p>
+        <b-button variant="danger" href="#">Mas Info</b-button>
+      </b-jumbotron>
+    </div>
+
+
+    <template>
+      <div>
+        <b-progress :value="value" :max="max" variant="danger" show-progress animated></b-progress>
+        <b-button variant="danger" class="mt-3" @click="randomValue">Click me</b-button>
+      </div>
+    </template>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+  return {
+    value: 45,
+    max: 100
+  }
+},
+methods: {
+  randomValue() {
+    this.value = Math.random() * this.max
+  }// cierre random
+},//fin methods
+}// fin export
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
